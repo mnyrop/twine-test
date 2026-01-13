27b78445119e366970ab5b694a32969ff0660c37
mnyrop <marii@nyu.edu>
2026-01-13 12:34:05 -0500

    init mvp

---
+++ build.js
@@ -0,0 +1,20 @@
+const { exec }  = require('child_process');
+const fs        = require('fs');
+const glob      = require('glob');
+var path        = require('path');
+
+const format = 'harlowe/3.3.9';
+const files  = glob.sync("src/stories/**/*.twee");
+
+console.log(`Building stories with ${format}...\n`);
+
+for (const file of files) {
+  const target = file.replace("src/stories/", "dist/").replace(".twee", ".html");
+  const dir    = path.dirname(target);
+  
+  fs.mkdirSync(dir, { recursive: true });   
+  exec(`extwee -c -i ${file} -s 'lib/story-formats/${format}/format.js' -o ${target}`);
+  console.log(`Built ${target}`);
+}
+
+console.log('\nBuild complete! ✅');
